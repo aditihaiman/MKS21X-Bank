c83c4aeb158e50ba18698fc25d3bf0c7ecdde1d1
Aditi Haiman <aditi@Aditis-MacBook-Air.local>
2018-10-03 17:43:27 -0400

    added comments

--- BankDriver.java
+++ BankDriver.java
@@ -2,13 +2,13 @@ public class BankDriver{
 	public static void main(String[] args) {
 		BankAccount myAccount = new BankAccount(10000, 242225217, "abadpassword");
 
-		System.out.println(myAccount.getBalance());
-		System.out.println(myAccount.getPassword());
-		System.out.println(myAccount.getID());
+		System.out.println(myAccount.getBalance());  //returns: 10000.0
+		System.out.println(myAccount.getPassword()); //returns: abadpassword
+		System.out.println(myAccount.getID()); //returns: 242225217
 		myAccount.deposit(499);
-		System.out.println(myAccount.getBalance());
+		System.out.println(myAccount.getBalance()); //returns: 10499.0
 		myAccount.withdraw(4319);
-		System.out.println(myAccount.getBalance());
+		System.out.println(myAccount.getBalance()); //returns: 6180.0
 
 	}
 
