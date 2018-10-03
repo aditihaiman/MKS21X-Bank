f1e6c59b4181e3c33dd627291692c71bde7de8ce
Aditi Haiman <ahaiman00@stuy.edu>
2018-10-03 15:55:45 -0400

    edited BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,27 @@
 public class BankAccount{
 	private double balance;
+	private int accountID;
+	private String password;
 	
-
-
-
-
-
+	public BankAccount(double newBalance, int newID, String newPass){
+		balance = newBalance;
+		accountID = newID;
+		password = newPass;
+	}
+	public String toString() {
+		return accountID + "\t" + balance;
+	}
+	public boolean deposit(double amount) {
+		if (amount >= 0){			
+			double += amount;
+			return true;
+		}
+		else {return false;}
+	}
+	public boolean withdraw(double amount) {
+		if (amount > balance) return false;
+		else {
+			balance -= amount;
+			return true;
+		}
 }
